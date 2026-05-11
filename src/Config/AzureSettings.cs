804b14be3e21fb041842e564b3070455451e14a6
SHAKIR, Muhammad <Muhammad.SHAKIR@EDUCATION.GOV.UK>
2026-05-11 12:25:02 +0100

    Updated dockerfile and creared deployment workflow

--- src/Config/AzureSettings.cs
+++ src/Config/AzureSettings.cs
@@ -7,8 +7,6 @@ public class AzureSettings
     public string AzureOpenaiEndpoint { get; set; } = string.Empty;
     public string AzureSearchEndpoint { get; set; } = string.Empty;
     public string AzureOpenaiDeployment { get; set; } = string.Empty;
-    public string AzureEmbeddingEndpoint { get; set; } = string.Empty;
-    public string AzureEmbeddingKey { get; set; } = string.Empty;
     public string AzureEmbeddingDeployment { get; set; } = string.Empty;
     public string AzureSearchIndex { get; set; } = string.Empty;
 
